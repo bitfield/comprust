5ad975b4e23a741ecfa7e93e9a0de6a24c4f61b0
John Arundel <john@bitfieldconsulting.com>
2024-06-11 12:05:45 +0100

    add rot13

---
+++ tests/rot13.rs
@@ -0,0 +1,61 @@
+use std::io::{Read, Result};
+
+struct RotDecoder<R: Read> {
+    input: R,
+    rot: u8,
+}
+
+impl<R> Read for RotDecoder<R>
+where
+    R: Read,
+{
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let n = self.input.read(buf)?;
+        for b in &mut buf[..n] {
+            if b.is_ascii_alphabetic() {
+                let base = if b.is_ascii_lowercase() { 'a' } else { 'A' } as u8;
+                *b = (*b - base + self.rot) % 26 + base;
+            }
+        }
+        Ok(n)
+    }
+}
+
+#[test]
+fn main() {
+    let mut rot = RotDecoder {
+        input: "Gb trg gb gur bgure fvqr!".as_bytes(),
+        rot: 13,
+    };
+    let mut result = String::new();
+    rot.read_to_string(&mut result).unwrap();
+    println!("{result}");
+}
+
+#[test]
+fn joke() {
+    let mut rot = RotDecoder {
+        input: "Gb trg gb gur bgure fvqr!".as_bytes(),
+        rot: 13,
+    };
+    let mut result = String::new();
+    rot.read_to_string(&mut result).unwrap();
+    assert_eq!(&result, "To get to the other side!");
+}
+
+#[test]
+fn binary() {
+    let input: Vec<u8> = (0..=255u8).collect();
+    let mut rot = RotDecoder::<&[u8]> {
+        input: input.as_ref(),
+        rot: 13,
+    };
+    let mut buf = [0u8; 256];
+    assert_eq!(rot.read(&mut buf).unwrap(), 256);
+    for i in 0..=255 {
+        if input[i] != buf[i] {
+            assert!(input[i].is_ascii_alphabetic());
+            assert!(buf[i].is_ascii_alphabetic());
+        }
+    }
+}
